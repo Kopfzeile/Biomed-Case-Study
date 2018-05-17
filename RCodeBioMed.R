9dccfbf147c8467227bbb339b05789641a3a3ebb
Katie <katalinroth@hotmail.com>
2018-05-17 10:18:21 +0200

    stuff

--- RCodeBioMed.R
+++ RCodeBioMed.R
@@ -4,7 +4,7 @@
 
 
 ## Datafile preparation
-setwd("/Users/tikkaoo/Dropbox/UNI 2017-2018/Biomedical Statistics/Case Study")
+setwd("/Users/tikkaoo/Documents/GitHub/Biomed-Case-Study")
 dat <- read.table("otitismedia.csv", sep = ",", header = TRUE)
 summary(dat)
 str(dat)
@@ -12,4 +12,9 @@ str(dat)
 # Dependent variable: OM_after6mo 
 # Independent variable: BMI_6mo
 # Confounders: all the others.. 
+hist(dat$OM_after6mo) # lot of zeros, but not that many -> sweked distribution
+hist(dat$fup_6mo) # most kids followed up for same time, some dropped out
 
+hist(dat$BMI_6mo) # such a nice normal distribution
+
+# Recode sex variable
